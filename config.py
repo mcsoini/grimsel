a0b472f148a8ea99619c377dfbd3a370f7d454f9
mcsoini <martin.soini@unige.ch>
2018-10-05 10:44:44 +0200

    update config

--- config.py
+++ config.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import grimsel
+
+
 
 try:
     import grimsel.config_local as conf_local
     PATH_CSV = conf_local.PATH_CSV
 except:
-    PATH_CSV = os.path.abspath('input_data')
+    PATH_CSV = os.path.join(grimsel.__path__[0], 'input_data')
     print('Using default csv path %s'%PATH_CSV)
 
 try:
@@ -16,12 +19,20 @@ try:
     DATABASE = conf_local.DATABASE
     SCHEMA = conf_local.SCHEMA
 
+    PSQL_USER = conf_local.PSQL_USER
+    PSQL_PASSWORD = conf_local.PSQL_PASSWORD
+    PSQL_HOST = conf_local.PSQL_HOST
+    PSQL_PORT = conf_local.PSQL_PORT
 except:
     raise RuntimeError('Please set configuration parameters in '
                        'grimsel/config_local.py, e.g. \n'
                        'FN_XLSX = os.path.abspath(\'../DATA/input.xlsx\')\n'
                        'DATABASE = \'database_name\'\n'
                        'SCHEMA = \'model_input_schema_name\'\n'
+                       'PSQL_USER = \'user\'\n'
+                       'PSQL_PASSWORD = \'password\'\n'
+                       'PSQL_PORT = 5432\n'
+                       'PSQL_HOST = \'localhost\'\n'
                        )
 
 
