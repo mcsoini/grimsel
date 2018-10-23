933d4098b433669ced96f351ddc6758a8a154b63
mcsoini <martin.soini@unige.ch>
2018-10-23 12:09:11 +0200

    fix cap_avlb definition

--- core/parameters.py
+++ core/parameters.py
@@ -78,6 +78,7 @@ class Parameters:
 
         print('Defining parameters for all generators')
         _df = self.df_plant_encar.copy()
+        _df['cap_avlb'] = 1 # init 1, adjusted from monthly correction factors
         _df = _df.loc[_df['pp_id'].isin(self.setlst['ppall'])]
         sets = (self.pp | self.pr | self.ror | self.st | self.hyrs |
                 self.curt | self.lin, self.ca)
@@ -85,7 +86,7 @@ class Parameters:
         self.padd('vc_om', sets, _df, **mut) # .
         self.padd('fc_om', sets, _df, **mut, default=0) # .
         self.padd('fc_dc', sets, _df, **mut) # .
-        self.padd('cap_avlb', sets, 1, **mut) # .
+        self.padd('cap_avlb', sets, _df, **mut) # .
 
         print('Defining parameters investment')
         _df = self.df_plant_encar.copy()
