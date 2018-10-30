0ae5769bbcae86f2890daca51f60c1f9b98da397
mcsoini <martin.soini@unige.ch>
2018-10-30 19:12:01 +0100

    MAJOR: linear variable costs are now factors to be multiplied with the fuel costs
    allows to keep fuel and plant properties separate

--- core/parameters.py
+++ core/parameters.py
@@ -70,8 +70,8 @@ class Parameters:
 
         _df = self.df_plant_encar.copy()
         _df = _df.loc[_df.pp_id.isin(self.setlst['lin'])]
-        self.padd('vc_fl_lin_0', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
-        self.padd('vc_fl_lin_1', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
+        self.padd('factor_vc_fl_lin_0', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
+        self.padd('factor_vc_fl_lin_1', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
         self.padd('factor_vc_co2_lin_0', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
         self.padd('factor_vc_co2_lin_1', (self.lin, self.ca), _df, default=0, **mut) # EUR/MWh_el
         self.padd('price_co2', (self.nd,), 'df_def_node', **mut) # EUR/MWh_el
