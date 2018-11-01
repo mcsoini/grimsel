c469e8b8ba87926251b8f3959b819d72285d05b2
mcsoini <martin.soini@unige.ch>
2018-11-01 17:47:28 +0100

    exclude hydro boundary conditions parameter from writing

--- core/io.py
+++ core/io.py
@@ -102,7 +102,7 @@ class IO():
         ('vc_ramp', ('pp_id', 'ca_id')),
         ('st_lss_hr', ('pp_id', 'ca_id')),
         ('st_lss_rt', ('pp_id', 'ca_id')),
-        ('hyd_erg_bc', ('sy', 'pp_id')),
+#        ('hyd_erg_bc', ('sy', 'pp_id')), # this gets extremely large
         ('hyd_erg_min', ('pp_id',)),
         ('inflowprof', ('sy', 'pp_id', 'ca_id')),
         ('chpprof', ('sy', 'nd_id', 'ca_id')),
