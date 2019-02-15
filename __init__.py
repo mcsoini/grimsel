085e502f4485b5cf4f4b7bb8989139aaf474e721
mcsoini <soini@posteo.de>
2019-02-15 10:44:58 +0100

    grimsel.__init__: clean up

--- __init__.py
+++ __init__.py
@@ -1,16 +1,6 @@
-'''
-		Welcome to GRIMSEL_H,
-		    your
-	  GeneRal Integrated Modeling environment
-					for the
-		Supply of Electricity
-				and Low-temperature Heat
-'''
-
 
 #__all__ = ['auxiliary', 'core', 'plotting', 'analysis']
 
-#print(__doc__)
 import logging
 
 def _get_logger(name):
