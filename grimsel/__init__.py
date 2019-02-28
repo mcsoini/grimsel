0f77a03102dd7138f9d6958f1bcd1ac1205556b5
mcsoini <soini@posteo.de>
2019-02-28 10:57:20 +0100

    grimsel __init__

---
+++ grimsel/__init__.py
@@ -0,0 +1,17 @@
+
+import logging
+
+def _get_logger(name):
+    logger = logging.getLogger(name)
+
+    if not logger.hasHandlers():
+        f_handler = logging.StreamHandler()
+        f_handler.setLevel(0)
+        format_str = '> %(asctime)s - %(levelname)s - %(name)s - %(message)s'
+        f_format = logging.Formatter(format_str, "%H:%M:%S")
+        f_handler.setFormatter(f_format)
+        logger.addHandler(f_handler)
+
+    return logger
+
+logger = _get_logger(__name__)
