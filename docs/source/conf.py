45cfad7f2abdaa15190e8ead0a74b4af94bfd41c
mcsoini <soini@posteo.de>
2019-02-28 12:45:00 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -129,7 +129,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 MOCK_MODULES = ["Pyomo", "pyomo"]
-sys.modules.update((mod_name, Mock) for mod_name in MOCK_MODULES)
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Options for LaTeX output ------------------------------------------------
 
