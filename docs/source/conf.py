7a7127ae03a162fe57636dff56c5aa96dc053b00
mcsoini <soini@posteo.de>
2019-02-28 11:57:09 +0100

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ autodoc_member_order = 'bysource'
 
 print('Setting autodoc_mock_imports')
 autodoc_mock_imports = ["tables", "Pyomo", "pyomo", 
-                        "statsmodels",
+                        "statsmodels", "psycopg2", 
                         "psycopgy2"]
 print(autodoc_mock_imports)
 
