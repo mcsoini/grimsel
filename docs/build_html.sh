7e63b38e87ee2c8478162ca257a75d80707fa2de
mcsoini <soini@posteo.de>
2020-02-17 12:15:16 +0100

    doc update

--- docs/build_html.sh
+++ docs/build_html.sh
@@ -5,6 +5,8 @@ mv ../notebooks/doc_running_grimsel.rst ./source/
 
 jupyter nbconvert ../notebooks/doc_introductory_example.ipynb --to rst 
 mv ../notebooks/doc_introductory_example.rst ./source/
+mv ../notebooks/doc_introductory_example_files/* ./source/_static
+sed -i 's/doc_introductory_example_files/_static/g' source/doc_introductory_example.rst
 
 make html
 
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,8 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.append('/mnt/data/Dropbox/GRIMSEL_SOURCE/grimsel')
 
 
 # -- Project information -----------------------------------------------------
